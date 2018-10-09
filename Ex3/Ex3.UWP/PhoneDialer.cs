851c628b60fc2205e4dac998c7e98c1a5ce7ae42
Özgür ÖZŞEN <oozsen@anadolu.edu.tr>
2018-10-09 15:05:31 +0300

    Add Windows Mobile Extensions for the UWP

--- Ex3/Ex3.UWP/PhoneDialer.cs
+++ Ex3/Ex3.UWP/PhoneDialer.cs
@@ -1,9 +1,6 @@
 ﻿using Ex3.UWP;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
+using Windows.Foundation.Metadata;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(PhoneDialer))]
@@ -14,7 +11,13 @@ namespace Ex3.UWP
     {
         public Task<bool> DialAsync(string number)
         {
-            throw new NotImplementedException();
+            //throw new NotImplementedException();
+            if (ApiInformation.IsApiContractPresent("Windows.ApplicationModel.Calls.CallsPhoneContract",1,0))
+            {
+                Windows.ApplicationModel.Calls.PhoneCallManager.ShowPhoneCallUI(number, "Phoneword");
+                return Task.FromResult(true);
+            }
+            return Task.FromResult(false);
         }
     }
 }
